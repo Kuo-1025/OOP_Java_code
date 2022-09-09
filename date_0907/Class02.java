dd83e02324c2cf7d5184251cc4de98ddb0cbec1a
Jun-an <evan20021025@gmail.com>
2022-09-09 22:19:58 +0800

    update
    Class02 has bug

--- date_0907/Class02.java
+++ date_0907/Class02.java
@@ -1,11 +1,9 @@
-import java.util.*;
-
 class Cbbb {
     double x, y;
 }
 
 class Class02 {
-    public static void main(String[] args) {
+    public static void main(String[] argv) {
         Cbbb obj1 = new Cbbb();
         obj1.x = 5.2;
         obj1.y = 3.9;
@@ -18,8 +16,8 @@ class Class02 {
         avg.x = (obj1.x + obj2.x) / 2;
         avg.y = (obj1.y + obj2.y) / 2;
 
-        System.out.printf("obj1.x = %.f, obj1.y = %.f\n", obj1.x, obj1.y);
-        System.out.printf("obj2.x = %.f, obj2.y = %.f\n", obj2.x, obj2.y);
-        System.out.printf("avg.x = %.f, avg.y = %.f\n", avg.x, avg.y);
+        System.out.printf("obj1.x = %.1f, obj1.y = %.1f\n", obj1.x, obj1.y);
+        System.out.printf("obj2.x = %.1f, obj2.y = %.1f\n", obj2.x, obj2.y);
+        System.out.printf("avg.x = %.1f, avg.y = %.1f", avg.x, avg.y);
     }
 }
