728931b8ae68c4d09466a6f62d7bb250c5f98796
Jun-an <evan20021025@gmail.com>
2022-10-23 17:26:34 +0800

    update
    change "extends from" to "inherits"

--- Java_10/Class_01.java
+++ Java_10/Class_01.java
@@ -5,7 +5,7 @@ class Caaa {
 }
 
 class Cbbb extends Caaa {
-    // Creat a class and extends from Caaa -> Cbbb()
+    // Creat a class and inherits Caaa -> Cbbb()
 
     public void set_num(int b_num1, int b_num2) {
         // Set num1 and num2 extended from Caaa.
