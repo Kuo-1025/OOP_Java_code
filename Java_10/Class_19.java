775155223e96fabc55ad9c0c26bc621fc677cb0b
Jun-an <evan20021025@gmail.com>
2022-10-29 20:29:29 +0800

    update
    Add toString().
    Delete println().

--- Java_10/Class_19.java
+++ Java_10/Class_19.java
@@ -12,17 +12,16 @@ class CRectangle {
         length = l;
         width = w;
     }
+
+    public String toString() {
+        // (c)
+        return "length=" + length + ", width=" + width + ", area=" + length * width;
+    }
 }
 
 public class Class_19 {
     public static void main(String[] argv) {
         CRectangle rect = new CRectangle(2, 6); // (b)
-        println(rect); // (c)
-    }
-
-    public static void println(CRectangle obj) {
-        // (c)
-
-        System.out.println("length=" + obj.length + ", width=" + obj.width + ", area=" + obj.length * obj.width);
+        System.out.println(rect); // (c)
     }
 }
