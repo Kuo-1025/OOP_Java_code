f4447c4963aaec92f7bc1d467bf89e20b50e24af
Jun-an <evan20021025@gmail.com>
2022-12-06 21:54:15 +0800

    upload

---
+++ Java_12/pack6/subpack1/subsubpack/Cstyle.java
@@ -0,0 +1,14 @@
+package pack6.subpack1.subsubpack;
+
+public class Cstyle {
+
+    public int style;
+
+    public Cstyle(int n) {
+
+        style = n;
+
+        System.out.println("style" + style);
+
+    }
+}
