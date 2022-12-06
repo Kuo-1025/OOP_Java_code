76cf031accd0f1434c21d9f1b9e9f69e7dffed48
Jun-an <evan20021025@gmail.com>
2022-12-06 23:04:16 +0800

    upload

---
+++ Java_12/Class14.java
@@ -0,0 +1,10 @@
+public class Class14 {
+    public static void main(String[] argv) {
+
+        String sentence = "Habit is second nature.";
+
+        // Use substring() which is in String class to get "nature" from sentence.
+        System.out.println(sentence.substring(16, 22));
+
+    }
+}
