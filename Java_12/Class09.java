2904ed28595c142e2f5e786065522dc5fb7e491c
Jun-an <evan20021025@gmail.com>
2022-12-06 22:41:35 +0800

    update

--- Java_12/Class09.java
+++ Java_12/Class09.java
@@ -1,8 +1,9 @@
-import pack9.sub1.CSphere;
-import pack9.sub2.CTrapezoid;
+import pack9.sub1.CSphere; // Import class CSphere which is in pack9.sub1
+import pack9.sub2.CTrapezoid; // Import class CTrapezoid which is in pack9.sub2
 
 public class Class09 {
     public static void main(String[] argv) {
+        // (e) Test.
 
         CSphere CSp = new CSphere(69);
         CTrapezoid CTr = new CTrapezoid(48, 76, 3);
