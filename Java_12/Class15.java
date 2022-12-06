c99321cdaffb31252760a4b97cccc62b40c1d7fe
Jun-an <evan20021025@gmail.com>
2022-12-06 23:12:39 +0800

    upload

---
+++ Java_12/Class15.java
@@ -0,0 +1,12 @@
+public class Class15 {
+    public static void main(String[] argv) {
+
+        double d = 123.45;
+
+        /*
+         * Use valueOf() which is in String class to convert double to String
+         * and length() to get its length.
+         */
+        System.out.println(String.valueOf(d).length());
+    }
+}
