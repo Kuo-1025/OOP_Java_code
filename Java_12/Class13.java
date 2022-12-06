6778e7d9b90be215d4ba7edf874661a70385cda5
Jun-an <evan20021025@gmail.com>
2022-12-06 23:15:25 +0800

    update

--- Java_12/Class13.java
+++ Java_12/Class13.java
@@ -4,7 +4,7 @@ public class Class13 {
     public static void main(String[] argv) {
 
         // Print the integer closest to 5.38.
-        System.out.println((int) Math.floor(5.38));
+        System.out.println("The integer closest to 5.38 is " + (int) Math.floor(5.38) + '.');
 
     }
 }
