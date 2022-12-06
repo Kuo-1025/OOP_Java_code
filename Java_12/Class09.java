d10d8f6e9d85afcd1c930a90308fa79b6b21b3f7
Jun-an <evan20021025@gmail.com>
2022-12-06 22:40:56 +0800

    upload

---
+++ Java_12/Class09.java
@@ -0,0 +1,13 @@
+import pack9.sub1.CSphere;
+import pack9.sub2.CTrapezoid;
+
+public class Class09 {
+    public static void main(String[] argv) {
+
+        CSphere CSp = new CSphere(69);
+        CTrapezoid CTr = new CTrapezoid(48, 76, 3);
+
+        CSp.show();
+        CTr.show();
+    }
+}
