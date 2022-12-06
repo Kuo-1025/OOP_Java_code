45e2441ab17136796548c4511ecb12b12fa11024
Jun-an <evan20021025@gmail.com>
2022-12-06 23:45:43 +0800

    upload

---
+++ Java_12/Class17.java
@@ -0,0 +1,29 @@
+public class Class17 {
+    public static void main(String[] argv) {
+
+        System.out.println("The range of short is from " + Short.MIN_VALUE + " to " + Short.MAX_VALUE + '.');
+
+        String str = "262904713";
+        short res = 0;
+
+        // It'll show you an error about value of str is out of the range of short.
+        res = Short.parseShort(str);
+
+        /*
+         * And you can use try-catch to check first,
+         * if there's an error, it'll go to catch.
+         * 
+         * try {
+         * 
+         * res = Short.parseShort(str);
+         * 
+         * } catch (Exception e) {
+         * 
+         * System.out.println(str + " is out of the range of short ! ! !");
+         * 
+         * }
+         * 
+         * System.out.println(res);
+         */
+    }
+}
