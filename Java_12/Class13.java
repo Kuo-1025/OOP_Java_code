bbb2ae3c6f6f93ae25ca94b67eda6d4fd225bb7d
Jun-an <evan20021025@gmail.com>
2022-12-06 22:53:50 +0800

    upload

---
+++ Java_12/Class13.java
@@ -0,0 +1,10 @@
+import java.lang.Math;
+
+public class Class13 {
+    public static void main(String[] argv) {
+
+        // Print the integer closest to 5.38.
+        System.out.println((int) Math.floor(5.38));
+
+    }
+}
