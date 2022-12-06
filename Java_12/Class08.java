7dbda1beafaa47b4bc1ab2554ac8df59c7e9113f
Jun-an <evan20021025@gmail.com>
2022-12-06 22:04:31 +0800

    update

--- Java_12/Class08.java
+++ Java_12/Class08.java
@@ -1,6 +1,6 @@
-import pack6.subpack1.CCircle;
-import pack6.subpack2.CRectangle;
-import pack6.subpack1.subsubpack.Cstyle;
+import pack6.subpack1.CCircle; // Import class CCircle which is in pack6.subpack1
+import pack6.subpack2.CRectangle; // Import class CRectangle which is in pack6.subpack1
+import pack6.subpack1.subsubpack.Cstyle; // Import class Cstyle which is in pack6.subpack1.subsubpack
 
 public class Class08 {
     public static void main(String[] argv) {
