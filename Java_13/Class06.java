bc3e3fe23df47a205d1f7a524159cdfe67636cbf
Jun-an <evan20021025@gmail.com>
2022-12-14 11:38:31 +0800

    upload Class06.java

---
+++ Java_13/Class06.java
@@ -0,0 +1,15 @@
+public class Class06 {
+    public static void main(String[] argv) {
+        int num = 12;
+        int den[] = { 12, 0, 3, 0, 0, 4 };
+
+        for (int i = 0; i < 10; ++i) {
+
+            try {
+                System.out.println("ans=" + num / den[i] + '\n');
+            } catch (Exception e) {
+                System.out.println("捕捉到例外了\n");
+            }
+        }
+    }
+}
