ac1d70190ca9ba85e1361a0e5dafd12efba5f78c
Jun-an <evan20021025@gmail.com>
2022-12-14 10:23:31 +0800

    update Class03.java

--- Java_13/Class03.java
+++ Java_13/Class03.java
@@ -2,6 +2,7 @@ public class Class03 {
     public static void main(String[] argv) {
         int num = 12, den = 0, ans = 48763;
 
+        // Use try-catch-finally to avoid the problem about divid zero.
         try {
             ans = num / den;
         } catch (ArithmeticException e) {
