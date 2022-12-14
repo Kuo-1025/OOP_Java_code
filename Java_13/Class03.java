91cd52cc60f2f5eba72d3cc3e6448a58535009e4
Jun-an <evan20021025@gmail.com>
2022-12-14 10:21:31 +0800

    upload Class03.java

---
+++ Java_13/Class03.java
@@ -0,0 +1,15 @@
+public class Class03 {
+    public static void main(String[] argv) {
+        int num = 12, den = 0, ans = 48763;
+
+        try {
+            ans = num / den;
+        } catch (ArithmeticException e) {
+            ans = -1;
+        } finally {
+            System.out.printf("ans = %d\n", ans);
+            System.out.println("end of main()");
+        }
+
+    }
+}
