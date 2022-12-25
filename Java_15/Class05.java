41c2c3966d199832073e23f180e33040843e4ee7
Jun-an <evan20021025@gmail.com>
2022-12-25 21:45:23 +0800

    upload Class05.java

---
+++ Java_15/Class05.java
@@ -0,0 +1,30 @@
+class CTest extends Thread {
+    String id;
+
+    public CTest(String str) {
+        id = str;
+    }
+
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 0; j < 100000000; j++)
+                ;
+            System.out.println(id + " " + i);
+        }
+    }
+}
+
+public class Class05 {
+    public static void main(String[] argv) {
+        CTest hi = new CTest("Hello");
+        CTest bye = new CTest("Good bye");
+        CTest morning = new CTest("Good morning");
+        CTest night = new CTest("Good night");
+
+        hi.start();
+        bye.start();
+        morning.start();
+        night.start();
+        // Let them start at the same time.
+    }
+}
